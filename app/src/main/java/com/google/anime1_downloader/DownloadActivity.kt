65595b82fa754712bbcda407fa20c0dd2d7de276
yuusukealmal <spark9487111@gmail.com>
2024-07-11 18:13:38 +0800

    parse get anime episode

--- app/src/main/java/com/google/anime1_downloader/DownloadActivity.kt
+++ app/src/main/java/com/google/anime1_downloader/DownloadActivity.kt
@@ -1,13 +1,18 @@
 package com.google.anime1_downloader
 
 import android.os.Bundle
+import android.util.Log
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class DownloadActivity : Fragment() {
     override fun onCreateView(
@@ -22,7 +27,7 @@ class DownloadActivity : Fragment() {
 
 
         ok.setOnClickListener {
-            Toast.makeText(context, title.text, Toast.LENGTH_SHORT).show()
+            get_urls(title.text.toString())
         }
         cancel.setOnClickListener{
             title.text = ""
@@ -30,4 +35,47 @@ class DownloadActivity : Fragment() {
 
         return view
     }
+
+    fun get_urls(animeUrl:String){
+        val url_list = mutableListOf<String>()
+
+        if (animeUrl.contains(Regex("anime1.me/category/(.*?)", RegexOption.IGNORE_CASE))) {
+            url_list.addAll(animeSeason(animeUrl))
+        } else if (animeUrl.contains(Regex("anime1.me/[0-9]", RegexOption.IGNORE_CASE))) {
+            url_list += animeUrl
+        }
+    }
+
+    fun animeSeason(animeUrl:String): Collection<String> {
+        val urls = mutableListOf<String>()
+        val headersJson = JSONObject(getString(R.string.headers))
+        val headersMap = headersJson.keys().asSequence().associateWith { headersJson.getString(it) }
+
+        val connection = URL(animeUrl).openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+
+        for ((key, value) in headersMap) {
+            connection.setRequestProperty(key, value)
+        }
+        val response = connection.inputStream.bufferedReader().use { it.readText() }
+        connection.disconnect()
+
+        val document = Jsoup.parse(response)
+        val h2Elements = document.select("h2.entry-title")
+
+        for (element in h2Elements) {
+            val url = element.select("a[rel=bookmark]").attr("href")
+            urls.add(url)
+        }
+
+        val navPrevious = document.select("div.nav-previous")
+        if (navPrevious.isNotEmpty()) {
+            val nextUrl = navPrevious.select("a").attr("href")
+            urls.addAll(animeSeason(nextUrl))
+        }
+
+        urls.reverse()
+        return urls
+    }
 }
+
